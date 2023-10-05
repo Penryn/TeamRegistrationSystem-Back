ae4858788a1880682e0969fb42b199eb70deae9b
penryn <15158052130@163.com>
2023-10-05 13:58:44 +0800

    中间件设置

--- main.go
+++ main.go
@@ -1,17 +1,25 @@
 package main
 
 import (
+	"TeamRegistrationSystem-Back/app/midwares"
 	"TeamRegistrationSystem-Back/app/services/userService"
 	"TeamRegistrationSystem-Back/config/database"
 	"TeamRegistrationSystem-Back/config/router"
 	"log"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	database.Init()
 	r := gin.Default()
+	r.Use(cors.Default())
+	r.Use(midwares.RateLimitMiddleware(time.Second,100,100))
+	r.Use(midwares.ErrHandler())
+	r.NoMethod(midwares.HandleNotFound)
+	r.NoRoute(midwares.HandleNotFound)
 	userService.CreateAdministrator()
 	r.Static("/uploads", "./uploads")
 	router.Init(r)
