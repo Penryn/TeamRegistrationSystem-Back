3261dbfbb65c6ce8929eb196e6e79663dede163d
naininena <144109994+naininena@users.noreply.github.com>
2023-10-10 20:14:58 +0800

    jwt

--- app/utils/jwtutil.go
+++ app/utils/jwtutil.go
@@ -3,61 +3,58 @@ package utils
 import (
 	"errors"
 	"time"
-	"TeamRegistrationSystem-Back/config/config"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
+var Secret = []byte("xyz")
 
 // jwt过期时间
-const expiration = time.Hour*2
+const expiration = time.Hour * 2
 
-type Claims struct{
+type Claims struct {
 	UserID int
 	jwt.StandardClaims
 }
 
-func GenToken(userid int)(string,error){
+func GenToken(userid int) (string, error) {
 	//创建声明
-	secret :=config.Config.GetString("jwt.pass")
-	var Secret = []byte(secret)
-	a:=Claims{
-		UserID:userid,
+	a := Claims{
+		UserID: userid,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: time.Now().Add(expiration).Unix(),
-			IssuedAt: time.Now().Unix(),
-			Issuer: "gin-jwt-demo",
-			Id: "",
+			IssuedAt:  time.Now().Unix(),
+			Issuer:    "gin-jwt-demo",
+			Id:        "",
 			NotBefore: 0,
-			Subject: "",
+			Subject:   "",
 		},
 	}
 
 	//哈希方法创建签名
-	tt:=jwt.NewWithClaims(jwt.SigningMethodHS256,a)
-	tokenString, err := tt.SignedString(Secret)
-    if err != nil {
-        return "", err
-    }
-    return "Bearer " + tokenString, nil
+	tt := jwt.NewWithClaims(jwt.SigningMethodHS256, a)
+	// tokenString, err := tt.SignedString(Secret)
+	// if err != nil {
+	// 	return "", err
+	// }
+	// return "Bearer " + tokenString, nil
+	return tt.SignedString(Secret)
 
 }
 
-func ParseToken(tokenStr string)(*Claims,error){
-	secret :=config.Config.GetString("jwt.pass")
-	var Secret = []byte(secret)
+func ParseToken(tokenStr string) (*Claims, error) {
 	if len(tokenStr) > 7 && tokenStr[:7] == "Bearer " {
-        tokenStr = tokenStr[7:]
-    }
-	token,err:=jwt.ParseWithClaims(tokenStr,&Claims{},func (token *jwt.Token)(interface{},error){
-		return Secret,nil
-	} )
-	if err !=nil{
-		return nil,err
+		tokenStr = tokenStr[7:]
+	}
+	token, err := jwt.ParseWithClaims(tokenStr, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return Secret, nil
+	})
+	if err != nil {
+		return nil, err
 	}
 	//检验token
-	if claims,ok:=token.Claims.(*Claims);ok&&token.Valid{
-		return claims,nil
+	if claims, ok := token.Claims.(*Claims); ok && token.Valid {
+		return claims, nil
 	}
-	return nil,errors.New("invalid token")
+	return nil, errors.New("invalid token")
 }
