a02e3b232fe87a85dfc45091740b36ad4e1848fb
naininena <144109994+naininena@users.noreply.github.com>
2023-10-12 07:56:18 +0800

    abaaba

---
+++ app/controller/adminController/admin.go
@@ -0,0 +1,77 @@
+package adminController
+
+import (
+	"TeamRegistrationSystem-Back/app/apiExpection"
+	"TeamRegistrationSystem-Back/app/services/userService"
+	"TeamRegistrationSystem-Back/app/utils"
+
+	//"TeamRegistrationSystem-Back/config/database"
+	//"TeamRegistrationSystem-Back/app/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func deleteUserAndMessages(c *gin.Context) error {
+	//获取用户身份token
+	//判断是否为管理员
+	//查询所在团队，是否存在正好5人的团体
+	//是->删除用户相关信息
+	//清空用户与团队的关联
+	//删除用户
+}
+
+type adminIdentify struct {
+	Permission int `json:"permission" binding:"Required"`
+}
+
+// 管理员界面
+func adminInterface(c *gin.Context) {
+	//获取用户身份token
+	n, er := c.Get("UserID")
+	if !er {
+		utils.JsonErrorResponse(c, 200, "token获取失败")
+		return
+	}
+	m, ok := n.(int)
+	if !ok {
+		utils.JsonErrorResponse(c, 200, "invalid user")
+		return
+	}
+	terr := userService.CheckUserExistByUID(m)
+	if terr != nil {
+		utils.JsonErrorResponse(c, 200, apiExpection.ParamError.Msg)
+		return
+	}
+
+	var data adminIdentify
+	err := c.ShouldBindJSON(&data)
+	if err != nil {
+		utils.JsonErrorResponse(c, 200, apiExpection.ParamError.Msg)
+		return
+	}
+	//判断操作权限
+	if data.Permission == 0 {
+		utils.JsonErrorResponse(c, 200, "insufficient privileges to perform the operation")
+		return
+	}
+
+	//我不知道他们有没有（
+	//还是说我应该从数据库拿
+	/*
+		var user models.User
+		database.DB.Where("uid = ?", m).First(&user)
+		permission := user.Permission
+		if permission==0 {
+			utils.JsonErrorResponse(c,200,"insufficient privileges to perform the operation")
+			return
+		}
+		//
+	*/
+	//?
+	allUserInfo, err := adminService.getAllUserInfo()
+	if err != nil {
+
+	}
+	allTeamInfo, errr := adminService.getAllTeamInfo()
+
+}
--- app/services/adminService/admin.go
+++ app/services/adminService/admin.go
@@ -1,10 +1,22 @@
-package messageService
+package adminService
 
 import (
 	"TeamRegistrationSystem-Back/app/models"
 	"TeamRegistrationSystem-Back/config/database"
 )
 
+/*
+	func isAdmin(UserID int)(int,error){
+		var user models.User
+		err := database.DB.First(&user, userID).Error
+		if err != nil {
+			return false, err
+		}
+		return user.Permission, nil
+	}
+
+}
+*/
 func getAllUserInfo() ([]models.Userinfo, error) {
 	var infoList []models.Userinfo
 	result := database.DB.Find(&infoList)
@@ -21,4 +33,13 @@ func getAllTeamInfo() ([]models.Team, error) {
 		return nil, result.Error
 	}
 	return infoList, nil
+}
+
+func deleteInfoByUserID(userID int) error {
+	//?
+	err := database.DB.Where("uid = ?", userID).Delete(&models.User{}).Error
+	if err != nil {
+		return err
+	}
+	return nil
 }
