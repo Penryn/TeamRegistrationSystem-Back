692110a802597e52d99294c69ca7538d1f8907b8
naininena <144109994+naininena@users.noreply.github.com>
2023-10-11 20:43:47 +0800

    阴暗扭曲地爬行

--- app/services/adminService/admin.go
+++ app/services/adminService/admin.go
@@ -5,38 +5,20 @@ import (
 	"TeamRegistrationSystem-Back/config/database"
 )
 
-// func getAllUserInfoHandler(w http.ResponseWriter,r* http.Request){
-
-// }
-func getAllUserInfo(userID []int) ([]models.Userinfo, error) {
-	// for i := 1; i <= len(userID) ; i++ {
-	//过滤出包含在userID中的记录
-	result := database.DB.Where("user_id IN?", userID).Find(&models.Userinfo{})
-	if result.Error != nil {
-		return nil, result.Error
-	}
+func getAllUserInfo() ([]models.Userinfo, error) {
 	var infoList []models.Userinfo
-	result = database.DB.Where("user_id IN?", userID).Find(&infoList)
+	result := database.DB.Find(&infoList)
 	if result.Error != nil {
 		return nil, result.Error
 	}
-	// }
 	return infoList, nil
 }
 
-func geyAllTeamInfo()
-
-/*
-func GetTeamMoreListByTeamID(teamID int) ([]models.Team, error) {
-	result := database.DB.Where("id=?", teamID).Find(&models.Team{})
+func getAllTeamInfo() ([]models.Team, error) {
+	var infoList []models.Team
+	result := database.DB.Find(&infoList)
 	if result.Error != nil {
 		return nil, result.Error
 	}
-	var teamList []models.Team
-	result = database.DB.Preload("Users").Where("id=?", teamID).Find(&teamList)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-	return teamList, nil
+	return infoList, nil
 }
-*/
