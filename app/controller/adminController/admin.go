838130dd403fc335696878e966a3e0a8ede43ecb
naininena <144109994+naininena@users.noreply.github.com>
2023-10-15 03:36:11 +0800

    我是傻逼

--- app/controller/adminController/admin.go
+++ app/controller/adminController/admin.go
@@ -12,96 +12,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-/*
-type GetInfoData struct {
-	Name string `form:"name" binding:"required"`
-}
-
-
-func deleteUserAndMessages(c *gin.Context) {
-	//获取用户身份token
-	n, er := c.Get("UserID")
-	if !er {
-		utils.JsonErrorResponse(c, 200, "token获取失败")
-		return
-	}
-	m, ok := n.(int)
-	if !ok {
-		utils.JsonErrorResponse(c, 200, "invalid user")
-		return
-	}
-	terr := userService.CheckUserExistByUID(m)
-	if terr != nil {
-		utils.JsonErrorResponse(c, 200, apiExpection.ParamError.Msg)
-		return
-	}
-
-	var now models.User
-	database.DB.Where("uid = ?", m).First(&now)
-	permission := now.Permission
-	if permission == 0 {
-		utils.JsonErrorResponse(c, 200, "insufficient privileges to perform the operation")
-		return
-	}
-
-	//获取要删除的用户
-	// var uid int
-	var username GetInfoData
-	err := c.ShouldBindQuery(&username)
-	if err != nil {
-		utils.JsonErrorResponse(c, 200, apiExpection.ParamError.Msg)
-		return
-	}
-	// var user models.User
-	// var uid int
-	user, qer := adminService.GetUserByName(username.Name)
-	if qer != nil {
-		utils.JsonErrorResponse(c, 200, "there's no such one person")
-		return
-	}
-	uid := user.UserID
-
-	//判断是否为管理员本人
-	if uid == m {
-		utils.JsonErrorResponse(c, 200, "No!!!I don't want to kill myself!")
-		return
-	}
-
-	//查询所在团队，是否存在已报名的团体
-	exs := adminService.CheckTeamExist(uid)
-	if exs == 1 {
-		por := adminService.CheckTeamByUserID(uid)
-		if por == 1 {
-			utils.JsonErrorResponse(c, 200, "already registed")
-			return
-		} else if por == 2 {
-			utils.JsonErrorResponse(c, 200, "teamCaptain nonono")
-			return
-		}
-	}
-
-	//否->清空用户与团队的关联
-	rerr := adminService.DeleteRelevantTeamInfo(uid)
-	if rerr != nil {
-		utils.JsonErrorResponse(c, 200, apiExpection.RequestError.Msg)
-		return
-	}
-
-	//删除用户相关信息
-	err = adminService.DeleteInfoByUserID(uid)
-	if err != nil {
-		utils.JsonErrorResponse(c, 200, apiExpection.RequestError.Msg)
-		return
-	}
-}
-*/
-
 // type adminIdentify struct {
 // 	Permission int `json:"permission" binding:"Required"`
 // }
 
 // 管理员界面
-func adminInterface(c *gin.Context) {
+func AdminInterface(c *gin.Context) {
 
 	//获取用户身份token
 	n, er := c.Get("UserID")
@@ -166,7 +82,7 @@ func adminInterface(c *gin.Context) {
 
 }
 
-func adminGetTeam(c *gin.Context) {
+func AdminGetTeam(c *gin.Context) {
 	//获取用户身份token
 	n, er := c.Get("UserID")
 	if !er {
@@ -213,6 +129,6 @@ func adminGetTeam(c *gin.Context) {
 	}
 }
 
-func adminNotice(c *gin.Context) {
+func AdminMessage(c *gin.Context) {
 
 }
--- config/router/router.go
+++ config/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"TeamRegistrationSystem-Back/app/controller/adminController"
 	"TeamRegistrationSystem-Back/app/controller/messageController"
 	"TeamRegistrationSystem-Back/app/controller/teamController"
 	"TeamRegistrationSystem-Back/app/controller/userController"
@@ -9,35 +10,43 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Init(r *gin.Engine){
+func Init(r *gin.Engine) {
 	const pre = "/api"
 
-	api:=r.Group(pre)
-	{	api.POST("/login",userController.Login)
-		api.POST("/reg",userController.Register)
-		api.PUT("/ret",userController.Retrieve)
-		api.POST("/email",userController.Sendmail)
-		user:=api.Group("/user").Use(midwares.JWTAuthMiddleware())
+	api := r.Group(pre)
+	{
+		api.POST("/login", userController.Login)
+		api.POST("/reg", userController.Register)
+		api.PUT("/ret", userController.Retrieve)
+		api.POST("/email", userController.Sendmail)
+		user := api.Group("/user").Use(midwares.JWTAuthMiddleware())
 		{
-			user.PUT("/info",userController.Updateinfodata)
-			user.POST("/avatar",userController.AvatarUpload)
-			user.GET("/info",userController.GetUserInfo)
-			user.GET("/message",messageController.GetUserInformation)
+			user.PUT("/info", userController.Updateinfodata)
+			user.POST("/avatar", userController.AvatarUpload)
+			user.GET("/info", userController.GetUserInfo)
+			user.GET("/message", messageController.GetUserInformation)
 		}
-		team:=api.Group("/team").Use(midwares.JWTAuthMiddleware())
+		team := api.Group("/team").Use(midwares.JWTAuthMiddleware())
 		{
-			team.POST("/create",teamController.CreateTeam)
-			team.DELETE("/delete",teamController.BreakTeam)
-			team.DELETE("/dismiss",teamController.DismissUser)
-			team.DELETE("",teamController.LeaveTeam)
-			team.POST("",teamController.JoinTeam)
-			team.GET("",teamController.SearchTeam)
-			team.GET("/info",teamController.GetTeamInfo)
-			team.PUT("/info",teamController.UpdateTeamInfo)
-			team.PUT("/cancel",teamController.CancelTeam)
-			team.PUT("/submit",teamController.SubmitTeam)
-			team.POST("/avatar",teamController.TeamAvatarUpload)
-			
+			team.POST("/create", teamController.CreateTeam)
+			team.DELETE("/delete", teamController.BreakTeam)
+			team.DELETE("/dismiss", teamController.DismissUser)
+			team.DELETE("", teamController.LeaveTeam)
+			team.POST("", teamController.JoinTeam)
+			team.GET("", teamController.SearchTeam)
+			team.GET("/info", teamController.GetTeamInfo)
+			team.PUT("/info", teamController.UpdateTeamInfo)
+			team.PUT("/cancel", teamController.CancelTeam)
+			team.PUT("/submit", teamController.SubmitTeam)
+			team.POST("/avatar", teamController.TeamAvatarUpload)
+
+		}
+		admin := api.Group("/admin").Use(midwares.JWTAuthMiddleware())
+		{
+			admin.GET("/user", adminController.AdminInterface)
+			admin.GET("/team", adminController.AdminGetTeam)
+			admin.DELETE("/delete", adminController.DeleteUserAndMessages)
+			admin.POST("/message", adminController.AdminMessage)
 		}
 
 	}
